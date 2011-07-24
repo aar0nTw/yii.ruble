0e59870fbc7f10dc9c920567294e265d43476540
aar0nTw <aaroms9733@gmail.com>
2011-07-25 04:25:18 +0800

    snippet add: 	Model:Relation snippet change: 	Yii::Before Save -> Model::Before Save

--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -1,4 +1,4 @@
-snippet 'Yii:Before Save' do |snip|
+snippet 'Model:Before Save' do |snip|
   snip.trigger = 'befsave'
   snip.expansion = '
 public function beforeSave()
@@ -13,4 +13,9 @@ public function beforeSave()
      }  
      return false;
 }'
+end
+
+snippet 'Model:Relation' do |snip|
+  snip.trigger = 'relate'
+  snip.expansion ='\'${1:name}\'=>array(self::${2:relation},\'${3:Model}\',\'${4:key}\',\'condition\'=>\'${5:condition}\'),'
 end
