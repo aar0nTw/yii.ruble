5c9d169c187cf79bc91bec01bcddb3652dfbeed3
aar0nTw <aaroms9733@gmail.com>
2011-07-27 00:40:19 +0800

    Criteria setting snippet add

--- bundle.rb
+++ bundle.rb
@@ -8,14 +8,14 @@ bundle do |bundle|
   bundle.menu 'Yii' do |main_menu|
     main_menu.command 'Search in Yii...'
     main_menu.command 'Search in Yii ClassRef...'
-    main_menu.command 'Paste and Swap Case'
-    main_menu.command 'Copy and Swap Case'
+    #main_menu.command 'Paste and Swap Case'
+    #main_menu.command 'Copy and Swap Case'
     main_menu.separator
     main_menu.menu 'Commands' do |sub_menu|
       sub_menu.command 'Swap Case'
     end
     main_menu.separator
-    main_menu.command 'params[...]'
+    main_menu.command 'params[\'...\']'
     main_menu.menu 'Models' do |sub_menu|
       sub_menu.command 'Model:Before Save'
       sub_menu.command 'Model:Relation'
--- snippets/controller.rb
+++ snippets/controller.rb
@@ -32,6 +32,8 @@ if(!Yii::app()->request->${1://request type}){
 ));'
   end
   
+  #Controller:Action
+  #Fast add a action in controller.
   snippet 'Controller:Action' do |snip|
     snip.trigger = 'action'
     snip.expansion = '
@@ -39,6 +41,21 @@ public function action${1:Name}()
 {
    \$this->render(\'${2:view}\');
 }    
+'
+  end
+  
+  #Add a criteria setting
+  #Fast to add a criteria property array.
+  snippet 'Add a Criteria setting' do |snip|
+    snip.trigger ='criteria'
+    snip.expansion = ' array(
+  \'select\'=>\'${1://the columns being selected.}\',
+  \'with\'  =>\'${2://the relational query criteria.}\',
+  \'condition\'=>\'${3://query condition}\',
+  \'group\' =>\'${4://how to group the query results.}\',
+  \'order\' =>\'${5://how to sort the query results.}\',
+  \'limit\' =>\'${6://maximum number of records to be returned.}\'
+ );
 '
   end
 end
